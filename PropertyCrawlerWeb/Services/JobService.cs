094b7d63bbe924e3fcc4917ab7166515ae89338d
Aldiger Mehilli <aldiger.m@scopicsoftware.com>
2019-06-15 14:17:59 +0200

    process repository

--- PropertyCrawlerWeb/Services/JobService.cs
+++ PropertyCrawlerWeb/Services/JobService.cs
@@ -1,6 +1,7 @@
 ﻿using Hangfire;
 using PropertyCrawler.Data;
 using PropertyCrawler.Data.Entity;
+using PropertyCrawler.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,41 +17,48 @@ namespace PropertyCrawlerWeb.Services
     public class JobService : IJobService
     { 
         private readonly ICrawlerService _crawlerService;
+        private readonly IProcessRepository _processRepository;
 
         public void Job(List<PostalCode> postalCodes, PropertyCrawler.Data.PropertyType propertyType, ProcessType processType)
         {
 
-           //var dateNow = DateTime.UtcNow;
-           // var result = new Process
-           // {
-           //     DateAdded = dateNow,
-           //     DateModified = dateNow,
-           //     Active = true,
-           //     Status = (int)ProcessStatus.Processing,
-           //     Type = processType
-           // };
-           // //insert process w
+           var dateNow = DateTime.UtcNow;
+            var result = new Process
+            {
+                DateAdded = dateNow,
+                DateModified = dateNow,
+                Active = true,
+                Status = (int)ProcessStatus.Processing,
+                Type = processType
+            };
+            //insert process w
 
-           // var jobId = BackgroundJob.Enqueue(() => _crawlerService.UrlCrawler(postalCodes, propertyType, processType));
+            var jobId = BackgroundJob.Enqueue(() => _crawlerService.UrlCrawler(postalCodes, propertyType, processType));
 
 
-           // //update process status
-           // BackgroundJob.ContinueJobWith(jobId, () => Execute(jobId, result.Id), JobContinuationOptions.OnAnyFinishedState);
+            //update process status
+            BackgroundJob.ContinueJobWith(jobId, () => Execute(jobId, result.Id), JobContinuationOptions.OnAnyFinishedState);
 
         }
         private void Execute(string jobId,int processId)
         {
-            //using (Hangfire.Storage.IStorageConnection connection = JobStorage.Current.GetConnection())
-            //{
-            //    Hangfire.Storage.JobData job = connection.GetJobData(jobId);
-            //    if (job.State == "Succeeded")
-            //    {
-            //        //var succeeded = connection.GetAllItemsFromSet($"batch:{batchId}:succeeded");
-            //        //if (succeeded.Any())
-            //        //{
-            //        //};
-            //    }
-            //}
+            using (Hangfire.Storage.IStorageConnection connection = JobStorage.Current.GetConnection())
+            {
+                Hangfire.Storage.JobData job = connection.GetJobData(jobId);
+                var process= _processRepository
+                if (job.State == "Succeeded")
+                {
+                    
+                    //var succeeded = connection.GetAllItemsFromSet($"batch:{batchId}:succeeded");
+                    //if (succeeded.Any())
+                    //{
+                    //};
+                }
+                else
+                {
+
+                }
+            }
         }
     }
 }
